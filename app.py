26af8fcb018e1045bcd5eb74d1e213bb84c36b8c
yadsashel <ysalhi600@gmail.com>
2025-05-11 19:07:01 +0100

    prepare for hosting

--- app.py
+++ app.py
@@ -226,4 +226,4 @@ def ping():
     return "pong", 200
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)  # Ensure debug=False for production
